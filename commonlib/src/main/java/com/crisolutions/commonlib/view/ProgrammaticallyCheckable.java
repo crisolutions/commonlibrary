7c3624427c46ecabe428109df855b85ac3532508
Parshav <parshav@live.com>
2018-10-31 14:16:50 -0500

    Removed Java from CommonLibrary

--- commonlib/src/main/java/com/crisolutions/commonlib/view/ProgrammaticallyCheckable.java
+++
@@ -1,9 +0,0 @@
-package com.crisolutions.commonlib.view;
-
-/**
- * Interface for UI widgets (particularly, checkable widgets) whose checked state can be changed programmatically,
- * without triggering the {@code OnCheckedChangeListener}
- */
-public interface ProgrammaticallyCheckable {
-    void setCheckedProgrammatically(boolean checked);
-}
